0c0a243d1a5ae6f857624ad5dc8a25ea6e71f08b
Jacek Kącki <jkacki@gmail.com>
2020-03-23 12:37:28 +0100

    hide login database

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0-ty2qk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
